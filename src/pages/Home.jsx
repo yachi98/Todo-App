f04bd2c2248c1ec0747c98b14e7236f21e316fa0
Daniel Yachnikov Hughes <daniel.lloyd.hughes@gmail.com>
2023-12-22 23:42:07 +0000

    changed background colour to black

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -13,7 +13,7 @@ const Home = () => {
   const divRef = useRef(null);
   const [showFilters, setShowFilters] = useState(false);
   const [isPowerMode, setIsPowerMode] = useState(false);
-  const [backgroundColor, setBackgroundColor] = useState("green"); // test
+  const [backgroundColor, setBackgroundColor] = useState("black");
 
   // const displayTask = tasks.find((element) => !element.isCompleted);
 
